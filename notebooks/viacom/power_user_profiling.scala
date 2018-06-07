344108f1895bdbec9ffcae88b1eace6aae34907e
Gaurav Soni <gaurav.soni226@gmail.com>
2018-06-07 16:48:56 +0530

    With corrrect Logic

--- notebooks/viacom/power_user_profiling.scala
+++ notebooks/viacom/power_user_profiling.scala
@@ -13,7 +13,7 @@ display(loadData)
 
 // COMMAND ----------
 
-val row=loadData.select("distinct_id","lr_birthdate","lr_gender","app_version","duration","duration_seconds","event","manufacturer")
+val row=loadData.select("user_type","distinct_id","lr_birthdate","lr_gender","app_version","duration","duration_seconds","event","manufacturer")
 
 // COMMAND ----------
 
@@ -21,7 +21,7 @@ row.show
 
 // COMMAND ----------
 
-val calcWT=row.filter($"event"==="Video Watched").select($"distinct_id",$"app_version",$"duration",$"duration_seconds",$"event", when(($"app_version" isin ("47","1.2.16","1.2.21")) and ($"duration" >= 0 and $"duration"<= 36000),"1").
+val calcWT=row.filter($"event"==="Video Watched" and $"user_type".isNotNull and $"user_type" =!= "Guest"). select($"distinct_id",$"app_version",$"duration",$"duration_seconds",$"event", when(($"app_version" isin ("47","1.2.16","1.2.21")) and ($"duration" >= 0 and $"duration"<= 36000),"1").
 when(!($"app_version" isin ("47","1.2.16","1.2.21")) and ($"duration_seconds" >0 and $"duration_seconds"<= 36000),"0").alias("dws")) .groupBy("distinct_id","dws").agg(when($"dws"==="1",sum("duration")).when($"dws"==="0",sum("duration_seconds"))).drop("dws").withColumnRenamed("CASE WHEN (dws = 1) THEN sum(duration) WHEN (dws = 0) THEN sum(duration_seconds) END","watchTime")
 
 // COMMAND ----------
@@ -34,18 +34,11 @@ val powerUserFlagTable=calcWT.withColumn("PowerUserFlag",when($"watchTime">1800,
 
 // COMMAND ----------
 
-import java.sql.Date
-import org.apache.spark.sql.types.{DateType, IntegerType,}
+import java.sql.Date._
+import java.util.Calendar
+import org.apache.spark.sql.types.{DateType, IntegerType}
 
 // COMMAND ----------
-
-val sysdate = Calendar.getInstance();
-var x=sysdate.get(Calendar.YEAR)
-val gender= loadData.filter($"distinct_id" === powerUserFlagTable.col("distinct_id")).
-select($"date_stamp_ist",$"distinct_id",$"lr_gender",$"manufacturer",$"lr_birthdate".cast(DateType),$"region", when($"lr_gender".isNull,"Other").otherwise($"lr_gender"), when($"lr_birthdate".isNotNull,minusUDF(year($"lr_birthdate".cast(DateType))))). drop("lr_gender").withColumnRenamed("CASE WHEN (lr_gender IS NULL) THEN Other ELSE lr_gender END","Gender").withColumnRenamed("CASE WHEN (lr_birthdate IS NOT NULL) THEN UDF(year(cast(lr_birthdate as date))) END","Age").groupBy("date_stamp_ist")
-
-// COMMAND ----------
-
 
 val minusUDF=udf((x:Int)=>{
   val sysdate = Calendar.getInstance();
@@ -55,7 +48,11 @@ var z=sysdate.get(Calendar.YEAR)
 
 // COMMAND ----------
 
-display(gender)
+val sysdate = Calendar.getInstance();
+var x=sysdate.get(Calendar.YEAR)
+val gender= loadData.filter($"distinct_id" === powerUserFlagTable.col("distinct_id")).
+select($"date_stamp_ist",$"distinct_id",$"lr_gender",$"manufacturer",$"lr_birthdate".cast(DateType),$"region", when($"lr_gender".isNull,"Other").otherwise($"lr_gender"), when($"lr_birthdate".isNotNull,minusUDF(year($"lr_birthdate".cast(DateType))))). drop("lr_gender").withColumnRenamed("CASE WHEN (lr_gender IS NULL) THEN Other ELSE lr_gender END","Gender").withColumnRenamed("CASE WHEN (lr_birthdate IS NOT NULL) THEN UDF(year(cast(lr_birthdate as date))) END","Age").groupBy("date_stamp_ist","manufacturer","region","Gender","Age").agg(countDistinct($"distinct_id"))
 
 // COMMAND ----------
 
+display(gender)
